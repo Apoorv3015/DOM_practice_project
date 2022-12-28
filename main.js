952a8d4f6af4e422c432d90eefe9a7c81e241aa8
apoorv3015 <apoorvposwal99@gmail.com>
2022-12-28 15:22:13 +0530

    Data added in Local Storage

--- main.js
+++ main.js
@@ -1,81 +1,11 @@
-// Single Element Selectors
-/*
-console.log(document.getElementById('my-form'));
-console.log(document.querySelector('.container'));
-// Multiple Element Selectors
-console.log(document.querySelectorAll('.item'));
-console.log(document.getElementsByTagName('li'));
-console.log(document.getElementsByClassName('item'));
+const myform = document.querySelector('#my-form');
 
-const items = document.querySelectorAll('.item');
-items.forEach((item) => console.log(item));
+myform.addEventListener('submit',addUser);
 
-
-// MANIPULATING THE DOM
-const ul = document.querySelector('.items');
-// ul.remove();
-// ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'Hello';
-ul.children[1].innerText = 'Brad';
-ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
-
-const btn = document.querySelector('.btn');
-// btn.style.background = 'red';
-
-
-// EVENTS
-
-// Mouse Event
-btn.addEventListener('click', e => {
+function addUser(e){
   e.preventDefault();
-  console.log(e.target.className);
-  document.getElementById('my-form').style.background = '#ccc';
-  document.querySelector('body').classList.add('bg-dark');
-  ul.lastElementChild.innerHTML = '<h1>Changed</h1>';
-});
-
-// Keyboard Event
-const nameInput = document.querySelector('#name');
-nameInput.addEventListener('input', e => {
-  document.querySelector('.container').append(nameInput.value);
-});
-
-*/
-
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
-
-myForm.addEventListener('submit', onSubmit);
-
-function onSubmit(e) {
-    //Prevent default
-    e.preventDefault();
-    
-    if(nameInput.value === '' || emailInput.value === '') {
-      // alert('Please enter all fields');
-      msg.classList.add('error');
-      msg.innerHTML = 'Please enter all fields';
-  
-      // Remove error after 3 seconds
-      setTimeout(() => msg.remove(), 3000);
-    } else {
-      // Create new list item with user
-      const li = document.createElement('li');
-  
-      // Add text node with input values
-      li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
-  
-      // Add HTML
-      // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
-  
-      // Append to ul
-      userList.appendChild(li);
-  
-      // Clear fields
-      nameInput.value = '';
-      emailInput.value = '';
-    }
-  }
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  localStorage.setItem('name',name);
+  localStorage.setItem('email',email);  
+}
